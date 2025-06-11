d608b5685a8aebf772ee07fbf05845b4d6fd7299
Brian Koser <brian@koser.us>
2025-06-11 16:35:44 -0500

    Initial commit

---
+++ hello-world/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine($"Hello, World! {DateTime.Now:MMMM dd, yyyy}");
+
+static IEnumerable<string> ReadFrom(string file)
+{
+    string? line;
+    using (var reader = File.OpenText(file))
+    {
+        while ((line = reader.ReadLine()) != null)
+        {
+            yield return line;
+        }
+    }
+}
+
+var lines = ReadFrom("sampleQuotes.txt");
+foreach (var line in lines)
+{
+    Console.WriteLine(line);
+}
